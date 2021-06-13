fd477ba46ee42180da932d2841567cc78da6d625
Ben <83777519+ilyBenny@users.noreply.github.com>
2021-06-13 10:04:55 +0000

    new home page

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,5 +1,25 @@
 document.querySelector("link[id='theme-css']").href = "assets/css/light.css";
 
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    document.querySelector("link[id='theme-css']").href = "assets/css/dark.css";
+  document.querySelector("link[id='theme-css']").href = "assets/css/dark.css";
+}
+
+function slideDown(element, delay) {
+  setTimeout(() => {
+    element.classList.remove("hidden")
+    element.classList.add("animate__animated")
+    element.classList.add("animate__fadeInDown")
+  }, delay)
+}
+
+function slideIn(element, delay) {
+  setTimeout(() => {
+    element.classList.remove("hidden")
+    element.classList.add("animate__animated")
+    element.classList.add("animate__fadeInUp")
+  }, delay)
+  setTimeout(() => {
+    element.classList.remove("animate__animated")
+    element.classList.remove("animate__fadeInUp")
+  }, delay + 1000)  
 }
