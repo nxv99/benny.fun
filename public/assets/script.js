93f9b061cad45914af15edae3bc67dabf5a57559
Ben <83777519+ilyBenny@users.noreply.github.com>
2021-06-13 14:37:41 +0000

    new home page pogger

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,8 +1,8 @@
-document.querySelector("link[id='theme-css']").href = "assets/css/light.css";
+// document.querySelector("link[id='theme-css']").href = "assets/css/light.css";
 
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  document.querySelector("link[id='theme-css']").href = "assets/css/dark.css";
-}
+// if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+//   document.querySelector("link[id='theme-css']").href = "assets/css/dark.css";
+// }
 
 function slideDown(element, delay) {
   setTimeout(() => {
@@ -23,3 +23,38 @@ function slideIn(element, delay) {
     element.classList.remove("animate__fadeInUp")
   }, delay + 1000)  
 }
+
+function toTopButton() {
+  if (
+    document.body.scrollTop > 300 || 
+    document.documentElement.scrollTop > 300 
+    ) {
+    document.getElementById("toTopButton").style.opacity = "100";
+  } else {
+    document.getElementById("toTopButton").style.opacity = "0";
+  }
+
+  setTimeout(() => {toTopButton()}, 1)
+}
+
+function elementInViewport(el) {
+  var top = el.offsetTop;
+  var left = el.offsetLeft;
+  var width = el.offsetWidth;
+  var height = el.offsetHeight;
+
+  while(el.offsetParent) {
+    el = el.offsetParent;
+    top += el.offsetTop;
+    left += el.offsetLeft;
+  }
+
+  return (
+    top < (window.pageYOffset + window.innerHeight) &&
+    left < (window.pageXOffset + window.innerWidth) &&
+    (top + height) > window.pageYOffset &&
+    (left + width) > window.pageXOffset
+  );
+}
+
+setTimeout(() => {toTopButton()}, 1)
