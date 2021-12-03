2b36122c948b35cb1f76bb31050b3b34f729269f
Ben Tettmar <83777519+bentettmar@users.noreply.github.com>
2021-12-03 18:43:20 +0000

    removwd

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ const app = express()
 app.use(express.static(__dirname + "/public/"))
 app.use('assets/', serveIndex(__dirname + '/assets/'))
 
-app.get("/search", (request, response) => {
-  response.sendFile(__dirname + "/public/search.html")
-})
-
 app.get("/github", (request, response) => {
   response.redirect("https://github.com/bentettmar")
 })
